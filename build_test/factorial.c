68074166dee1f259722e1614dafe07497f03a8ec
Jordi Blasco <jordi.blasco@gmail.com>
2022-04-27 12:56:56 +0200

    initial tests

---
+++ build_test/factorial.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+* file: factorial.c
+* OpenMP Example - factorial - C Version
+* Developed by Jordi Blasco <jordi.blasco@hpcnow.com>
+******************************************************************************/
+
+#include <omp.h>
+#include <stdio.h>
+
+int main(void){
+	int i,n=10;
+	int fac=1;
+#pragma omp parallel for reduction(*:fac)
+#pragma omp parallel for shared(n) private(i) reduction(*:fac)
+	for(i=2;i<=n;i++){
+		fac*=i;
+	}
+	printf("Thread%d - fac(%d)=%d\n",omp_get_thread_num(),n,fac);
+	return 0;
+}
